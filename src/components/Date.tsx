9c25d5db2a347ef0eaf77978d2b54c1cf3ba73f2
JerryU <jerry.uusitalo@gmail.com>
2024-01-27 22:20:17 +0200

    Create a getCurrentTime function that sets the placeholder value as current moment

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -3,6 +3,19 @@ interface DateProps {
 }
 
 const DateInput: React.FC<DateProps> = ({ handleDateAndTime }) => {
+
+    // Set the placeholder value to current time and day
+    const getCurrentTime = ()  : string => {
+        const currentDateandTime: Date = new Date()
+        const day = currentDateandTime.getUTCDate().toString().padStart(2,"0")
+        const month = (currentDateandTime.getUTCMonth() + 1).toString().padStart(2,"0")
+        const year = currentDateandTime.getUTCFullYear().toString()
+        const minute = currentDateandTime.getMinutes().toString().padStart(2,"0")
+        const hour = currentDateandTime.getHours().toString().padStart(2,"0")
+    
+        return `${year}-${month}-${day}T${hour}:${minute}`
+    }
+
     return (
         <div className="input-container">
             <label htmlFor="dateInput">Date</label>
@@ -10,6 +23,7 @@ const DateInput: React.FC<DateProps> = ({ handleDateAndTime }) => {
                 id="dateInput"
                 type="datetime-local"
                 data-test-id="orderTime"
+                defaultValue={getCurrentTime()}
                 onChange={handleDateAndTime}
             />
         </div>
